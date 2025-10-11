8600ee0035dd5da0b36e3e77a7b02979f951bcf9
SamAllen06 <samgallen06@gmail.com>
2025-10-11 14:37:50 -0400

    Feature: fahrenheitToCelsius method completed in Converter class

--- src/main/java/edu/bsu/cs/Converter.java
+++ src/main/java/edu/bsu/cs/Converter.java
@@ -1,8 +1,9 @@
 package edu.bsu.cs;
 
 public class Converter {
-    public static int fahrenheitToCelsius(int temp){
-        return (temp-32)*5/9;
+    public static int fahrenheitToCelsius(int fTemp){
+        float cTemp = (float) (fTemp-32)*5/9;
+        return Math.round(cTemp);
     }
 
 }
