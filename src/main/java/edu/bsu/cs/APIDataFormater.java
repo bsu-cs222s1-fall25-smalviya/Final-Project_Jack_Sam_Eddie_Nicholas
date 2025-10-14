027289e5dfaa3881852d63fce637bda085753867
eddie.juliancaballer@bsu.edu <eddiejuliancaballero@gmail.com>
2025-10-13 23:54:43 -0400

    Feature: Created Blank tests and APIDataFormater class

---
+++ src/main/java/edu/bsu/cs/APIDataFormater.java
@@ -0,0 +1,8 @@
+package edu.bsu.cs;
+
+public class APIDataFormater {
+
+    protected void WeatherApiParser(){
+
+    }
+}
---
+++ src/test/java/edu/bsu/cs/APIDataFormater.java
@@ -0,0 +1,12 @@
+package edu.bsu.cs;
+
+import org.junit.jupiter.api.Test;
+
+public class APIDataFormaterTest {
+
+    @Test
+    public void WeatherInformationParserTest(){
+        APIDataFormater apiDataFormater = new APIDataFormater();
+
+    }
+}
