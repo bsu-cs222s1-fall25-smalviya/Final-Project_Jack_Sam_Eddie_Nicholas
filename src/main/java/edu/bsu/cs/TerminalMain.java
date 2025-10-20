4134bb7ae1192108fb4610978b0fda85ccff4bc1
SamAllen06 <samgallen06@gmail.com>
2025-10-20 09:00:49 -0400

    Feature: created TerminalMain class

---
+++ src/main/java/edu/bsu/cs/TerminalMain.java
@@ -0,0 +1,9 @@
+package edu.bsu.cs;
+
+public class TerminalMain {
+    TerminalController terminalController = new TerminalController();
+
+    public static void main(String[] args){
+        terminalController.printIntroMessage();
+    }
+}
