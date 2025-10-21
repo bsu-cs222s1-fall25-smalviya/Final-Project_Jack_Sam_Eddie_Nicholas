a6a60d1eac276c51b5d371209833bc7c6eb2187b
NicholasB613817 <nicholasbraeman@gmail.com>
2025-10-21 09:55:19 -0400

    Feature: changed WeatherServiceAPI.java to now beingable to grab the URL, connection.connect may be reddundant code.

--- src/main/java/WeatherServiceAPI.java
+++ src/main/java/WeatherServiceAPI.java
@@ -1,26 +1,16 @@
 import java.io.IOException;
 import java.net.*;
-import java.nio.charset.Charset;
-
-package edu.bsu.cs;
 
 public class WeatherServiceAPI {
+    protected String createURLString(String latLong) {
+        return "https://api.weather.gov/points/" + latLong;
+    }
 
-    //Ensure you have proper internet connection in order to receive a connection to the API
-    protected URLConnection connectToGovernmentWeatherAPI(String title) throws IOException, URISyntaxException {
-        try {
-            String URL = "https://api.weather.gov/points/40.1933,-85.3863" +
-                    URLEncoder.encode(title, Charset.defaultCharset()) + "&rvprop=timestamp" + URLEncoder.encode("|", Charset.defaultCharset()) +
-                    "user&rvlimit=15&redirects";
-            URI uri = new URI(URL);
-            URLConnection connection = uri.toURL().openConnection();
-            connection.setRequestProperty("User-Agent", "Final-Project_Jack_Sam_Eddie_Nicholas/0.1 (Nicholas.braeman@bsu.edu)");
-            connection.connect();
-            return connection;
-        } catch (UnknownHostException e){
-
-            System.err.println("Connection refused: " + e.getMessage());
-            return null;
-        }
+    protected URLConnection getConnectionFromURL(String urlString) throws IOException {
+        URI uri = URI.create(urlString);
+        URLConnection connection = uri.toURL().openConnection();
+        //TODO: connection.connect(); potentially redundant
+        connection.connect();
+        return connection;
     }
 }
