ba1cf64797214df46136a3f4713236c651aa3c5b
NicholasB613817 <nicholasbraeman@gmail.com>
2025-10-28 20:52:16 -0400

    Feature: changed WeatherServiceAPITest to test for null of inputStream.

--- src/test/java/edu/bsu/cs/WeatherServiceAPITest.java
+++ src/test/java/edu/bsu/cs/WeatherServiceAPITest.java
@@ -1,43 +1,49 @@
 package edu.bsu.cs;
-
 import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URLConnection;
 
-//testing connection to URL and JSON in WeatherServiceAPI.java
 public class WeatherServiceAPITest {
 
     @Test
-    public void testWeatherServiceAPI() throws IOException{
+    public void testWeatherServiceAPI() throws IOException, ParseException {
         WeatherServiceAPI service = new WeatherServiceAPI();
-        //Muncie's coordination for testing
-        String latLong = "40.195923,85.387545";
+        //Muncie’s coordinates for testing
+        String latLong = "40.195923,-85.387545";
         String urlString = service.createURLString(latLong);
 
-        URLConnection connection = service.getConnectionFromURL(urlString);
+        InputStream connection = service.getInputStreamFromURL(urlString);
 
-        Assertions.assertNotNull(connection);
+        //if connection is null, it means it failed to grab data from API which means API isn't working, so no data
+        if (connection == null) {
+            Assertions.fail("Connection is null — failed to connect to the weather API.");
+            return; // Stop the test early
+        }
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        //it goes on only if the connection works
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection));
         StringBuilder response = new StringBuilder();
         String line;
         while ((line = reader.readLine()) != null) {
             response.append(line);
-        } //end of while
+        }
         reader.close();
 
         JSONParser parser = new JSONParser(JSONParser.MODE_PERMISSIVE);
         JSONObject json = (JSONObject) parser.parse(response.toString());
 
-        //API should return JSON object containing "properties"
+        //the API should return JSON object containing properties
         Assertions.assertTrue(json.containsKey("properties"));
-    } //end of testWeatherServiceAPI
-} //end of WeatherServiceAPITest class
+    }
+}
+
 
 
 
