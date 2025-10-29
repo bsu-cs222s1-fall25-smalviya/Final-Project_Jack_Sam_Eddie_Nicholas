64ecd666e68de59c876ce3bf091143081561a27c
SamAllen06 <samgallen06@gmail.com>
2025-10-28 21:35:27 -0400

    Fix: edited waether service api test
    simplified existing test in WeatherServiceAPITest and added another one to cover the other function

--- src/test/java/edu/bsu/cs/WeatherServiceAPITest.java
+++ src/test/java/edu/bsu/cs/WeatherServiceAPITest.java
@@ -11,36 +11,24 @@ import java.io.InputStreamReader;
 import java.net.URLConnection;
 
 public class WeatherServiceAPITest {
+    WeatherServiceAPI service = new WeatherServiceAPI();
 
     @Test
-    public void testWeatherServiceAPI() throws IOException, ParseException {
-        WeatherServiceAPI service = new WeatherServiceAPI();
+    public void testGetInputStreamFromURL() throws IOException, ParseException {
         //Muncie’s coordinates for testing
         String latLong = "40.195923,-85.387545";
         String urlString = service.createURLString(latLong);
 
         InputStream connection = service.getInputStreamFromURL(urlString);
 
-        //if connection is null, it means it failed to grab data from API which means API isn't working, so no data
-        if (connection == null) {
-            Assertions.fail("Connection is null — failed to connect to the weather API.");
-            return; // Stop the test early
-        }
+        Assertions.assertNotNull(connection);
+    }
 
-        //it goes on only if the connection works
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection));
-        StringBuilder response = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            response.append(line);
-        }
-        reader.close();
-
-        JSONParser parser = new JSONParser(JSONParser.MODE_PERMISSIVE);
-        JSONObject json = (JSONObject) parser.parse(response.toString());
-
-        //the API should return JSON object containing properties
-        Assertions.assertTrue(json.containsKey("properties"));
+    @Test
+    public void testCreateURLString(){
+        String expectedURL = "https://api.weather.gov/points/40.1933,-85.3863";
+        String actualURL = service.createURLString("40.1933,-85.3863");
+        Assertions.assertEquals(expectedURL, actualURL);
     }
 }
 
