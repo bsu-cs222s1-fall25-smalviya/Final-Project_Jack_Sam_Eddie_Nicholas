a67806f63a416decb56b7c54354cd7316311f893
eddie.juliancaballer@bsu.edu <eddiejuliancaballero@gmail.com>
2025-11-07 15:26:28 -0500

    Feature: Appended class to choose the most populous city

--- src/main/java/edu/bsu/cs/CitiesDatabaseParser.java
+++ src/main/java/edu/bsu/cs/CitiesDatabaseParser.java
@@ -1,13 +1,14 @@
 package edu.bsu.cs;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.*;
+
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import java.io.InputStream;
 
 public class CitiesDatabaseParser {
-    private final HashMap<String, String> zipCodeToCoordinates = new HashMap<>();
+    private final HashMap<String, List<String>> zipCodeToCoordinates = new HashMap<>();
 
     //At start of initialization immediately create HashMap values
     public CitiesDatabaseParser() throws IOException {
@@ -26,17 +27,29 @@ public class CitiesDatabaseParser {
             String zipCodeCell = dataFormatter.formatCellValue(row.getCell(15));
             String latitudeCell = dataFormatter.formatCellValue(row.getCell(6));
             String longitudeCell = dataFormatter.formatCellValue(row.getCell(7));
+            String populationCell = dataFormatter.formatCellValue(row.getCell(8));
 
             //Gives each individual zip code coordinates
             String[] zipCodes = zipCodeCell.split("\\s+");
             for (String zipCode : zipCodes) {
+                if (!zipCodeToCoordinates.containsKey(zipCode)) {
+                    zipCodeToCoordinates.computeIfAbsent(zipCode, _ -> new ArrayList<>())
+                            .addAll(Arrays.asList((latitudeCell + "," + longitudeCell),  populationCell));
+                } else {
+                    int storedPopulation = Integer.parseInt(zipCodeToCoordinates.get(zipCode).get(1));
 
-                zipCodeToCoordinates.put(zipCode, latitudeCell + "," + longitudeCell);
+                    if (Integer.parseInt(populationCell) > storedPopulation) {
+                        List<String> information = new ArrayList<>(List.of(latitudeCell + "," + longitudeCell, populationCell));
+                        zipCodeToCoordinates.put(zipCode,information);
+                    }
+
+                }
             }
         }
     }
 
     public String getCoordinates(String zipCode) {
-        return zipCodeToCoordinates.get(zipCode);
+        List<String> coordinates = zipCodeToCoordinates.get(zipCode);
+        return coordinates.getFirst();
     }
 }
